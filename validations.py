4438eaeba04cbd3a7b7d8ca81a5414035a295439
Mustafa Can Genç <samfatuu@gmail.com>
2022-09-08 17:13:35 +0300

    buy validation transformed to transaction validation

--- validations.py
+++ validations.py
@@ -2,20 +2,22 @@ from datetime import datetime
 
 date_format = "%d.%m.%Y"
 
-def is_valid_buy_commands(commands):
+def is_valid_transaction_commands(commands):
+  is_buy = True if commands[0] == "buy" else False
+
   if (len(commands) != 5):
-    print('''Hatalı fon alım komutu girdiniz.'
-    Doğru komut formatı: buy TARIH(17.03.2022) FON_KODU(IPJ) ADET(3452) FIYAT(4.123643)''')
+    print(f'''Hatalı fon {'alış' if is_buy else 'satış'} komutu girdiniz.'
+    Doğru komut formatı: {'buy' if is_buy else 'sell'} TARIH(17.03.2022) FON_KODU(IPJ) ADET(3452) FIYAT(4.123643)''')
     return False
 
   try:
     date = datetime.strptime(commands[1], date_format)
   except ValueError:
-    print("Alım tarihi yanlış formatta girildi. Doğru format: gün.ay.yıl")
+    print(f"{'Alış' if is_buy else 'Satış'} tarihi yanlış formatta girildi. Doğru format: gün.ay.yıl")
     return False
 
   if (date > datetime.now()):
-    print("Alım tarihi bugünden ileri bir tarihte olamaz.")
+    print(f"{'Alış' if is_buy else 'Satış'} tarihi bugünden ileri bir tarihte olamaz.")
     return False
 
   if (len(commands[2]) != 3):
